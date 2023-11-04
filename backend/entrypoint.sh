57e7c890b8d9af19201c3add80ecffc41d1859e1
bartosz-bok <bartos.bok@gmail.com>
2023-11-04 22:05:49 +0100

    add containerization

---
+++ backend/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+check_db_connection() {
+    while ! mysqladmin ping -h mysql --silent; do
+        echo "Czekam na gotowość bazy danych..."
+        sleep 1
+    done
+    echo "Baza danych postawiona. Próbuję zainicjować tabele oraz wgrać dane początkowe."
+}
+
+check_db_connection
+
+# Wykonanie skryptów SQL przy uruchamianiu kontenera MySQL
+mysql -h mysql -P 3306 -u root -p db --password=password < /sql_scripts/init_db.sql
+mysql -h mysql -P 3306 -u root -p db --password=password < /sql_scripts/fill_example.sql &
+
+echo "Zainicjowano tabele oraz wgrano dane początkowe."
+echo "Stawiam serwer..."
+
+# Uruchomienie serwera PHP
+php -S 192.168.9.151:20951
+
--- backend/server.php
+++ backend/server.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "127.0.0.1";
+$servername = "mysql";
 $username = "user";
 $password = "password";
 $dbname = "db";
