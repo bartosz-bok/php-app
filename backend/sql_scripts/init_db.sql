04fba63508cffd42063a3ba173b82bebc5a4c84e
bartosz-bok <bartos.bok@gmail.com>
2023-11-04 01:17:51 +0100

    add foreign key

--- backend/sql_scripts/init_db.sql
+++ backend/sql_scripts/init_db.sql
@@ -1,3 +1,10 @@
+CREATE TABLE categories (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            category_name VARCHAR(255),
+                            category_color VARCHAR(20)
+);
+
+
 CREATE TABLE events (
                         id INT AUTO_INCREMENT PRIMARY KEY,
                         event_name VARCHAR(255),
@@ -5,13 +12,8 @@ CREATE TABLE events (
                         end_date DATE,
                         description TEXT,
                         image_url VARCHAR(255),
-                        category_id INT
-);
-
-CREATE TABLE categories (
-                            id INT AUTO_INCREMENT PRIMARY KEY,
-                            category_name VARCHAR(255),
-                            category_color VARCHAR(20)
+                        category_id INT,
+                        FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE users (
