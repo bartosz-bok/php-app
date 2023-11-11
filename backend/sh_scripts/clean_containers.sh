0e305e17117414bd084d392ba846197c65cddc77
bartosz-bok <bartos.bok@gmail.com>
2023-11-11 13:43:30 +0100

    docker updates

--- backend/sh_scripts/clean_containers.sh
+++ backend/sh_scripts/clean_containers.sh
@@ -7,8 +7,8 @@ echo ""
 
 if [[ "$1" == "--remove-app" && "$2" == "--remove-mysql" ]]; then
     docker rmi mysql
-    docker rmi app-backend-container:latest
-    docker rmi app-frontend-container:latest
+    docker rmi backend-app-backend:latest
+    docker rmi backend-app-frontend:latest
 
     sudo rm -r mysql-data
     sudo service mysql stop
@@ -16,7 +16,8 @@ if [[ "$1" == "--remove-app" && "$2" == "--remove-mysql" ]]; then
     echo "Both application and MySQL images have been removed."
 elif [ "$1" == "--remove-app" ]; then
     docker rmi backend-app:latest
-    docker rmi app-frontend-container:latest
+    docker rmi backend-app-backend:latest
+    docker rmi backend-app-frontend:latest
 
     echo "Application image removed."
 elif [ "$1" == "--remove-mysql" ]; then
